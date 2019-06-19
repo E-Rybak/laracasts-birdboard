47f79fee2e1deb1ca89082015ba309272ccda1cf
Emanuel Rybak <ery@pc-tec.local>
2019-06-19 12:24:19 +0200

    Completed lesson 5 A Project Requires an Owner

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project index</title>
+</head>
+<body>
+<h1>Birdboard</h1>
+
+<ul>
+	@forelse($projects as $project)
+
+		<a href="{{ $project->path() }}"><li>{{$project->title}}</li></a>
+
+		@empty
+		<li>No projects yet</li>
+
+	@endforelse
+</ul>
+</body>
+</html>
